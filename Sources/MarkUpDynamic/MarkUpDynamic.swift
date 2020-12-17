33b51cf836233bb962994ee9b897d9ef63e63cf4
coe <coe.app@gmail.com>
2020-12-17 16:37:02 +0900

    initial commit

--- Sources/MarkUpDynamic/MarkUpDynamic.swift
+++ Sources/MarkUpDynamic/MarkUpDynamic.swift
@@ -1,3 +1,115 @@
-struct MarkUpDynamic {
-    var text = "Hello, World!"
+//
+//  MarkUpDynamic.swift
+//
+//
+//  Created by 日向 強 on 2020/12/15.
+//
+
+import Foundation
+
+@dynamicMemberLookup
+public class MarkUpDynamic {
+    let tag: String
+    var attributes: String?
+    var addEndTag = true
+    var inside: Inside
+    var queue:[MarkUpDynamic]
+    
+    public subscript(inside inside: MarkUpDynamic) -> MarkUpDynamic {
+        self.inside = .markUp(inside)
+        return self
+    }
+    
+    public subscript(attributes attributes: [String: String?]) -> MarkUpDynamic {
+        self.attributes = attributes.reduce("", { (result, arg1) -> String in
+            let (key, value) = arg1
+            if let value = value {
+                return result + String(format: #" %@="%@""#, key, value)
+            } else {
+                return result + String(format: #" %@"#, key)
+            }
+        })
+        
+        return self
+    }
+    
+    public subscript(character character: String) -> MarkUpDynamic {
+        self.inside = .character(character)
+        return self
+    }
+    
+    public subscript(addEndTag addEndTag: Bool) -> MarkUpDynamic {
+        self.addEndTag = addEndTag
+        return self
+    }
+    
+    public subscript(dynamicMember member: String) -> MarkUpDynamic {
+        queue.append(self)
+        return MarkUpDynamic(tag: member,queue: queue)
+    }
+    
+    public init(doctype:String) {
+        self.tag = ""
+        self.inside = .root(doctype)
+        self.queue = []
+    }
+    
+    public init() {
+        self.tag = ""
+        self.inside = .root("")
+        self.queue = []
+    }
+    
+    private init(tag: String, queue:[MarkUpDynamic]) {
+        self.tag = tag
+        self.queue = queue
+        self.inside = .character("")
+    }
+    
+    public func generate() -> String {
+        queue.append(self)
+        var str = ""
+        queue.forEach { (html) in
+            switch html.inside {
+            case .root(let docType):
+                str += docType
+            case .markUp,.character:
+                if addEndTag {
+                    str += String(format: "<%@%@>%@</%@>", html.tag, html.attributes ?? "", html.inside.generate(), html.tag)
+                } else {
+                    str += String(format: "<%@%@>", html.tag, html.attributes ?? "")
+                }
+                
+            }
+            
+        }
+        return str
+    }
+}
+
+enum Inside: CustomDebugStringConvertible {
+    var debugDescription: String {
+        switch self {
+        case .markUp(let markUp):
+            return "markUp:\(markUp.tag)"
+        case .character(let character):
+            return "character:\(character)"
+        case .root(let doctype):
+            return "root:\(doctype)"
+        }
+    }
+    case root(String)
+    case markUp(MarkUpDynamic)
+    case character(String)
+    
+    func generate() -> String {
+        switch self {
+        case .markUp(let markUp):
+            return markUp.generate()
+        case .character(let character):
+            return character
+        case .root:
+            return ""
+        }
+    }
 }
--- Tests/MarkUpDynamicTests/MarkUpDynamicTests.swift
+++ Tests/MarkUpDynamicTests/MarkUpDynamicTests.swift
@@ -3,13 +3,60 @@ import XCTest
 
 final class MarkUpDynamicTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MarkUpDynamic().text, "Hello, World!")
+        XCTAssertEqual(MarkUpDynamic()
+                        .html[inside: MarkUpDynamic()
+                                .body[inside: MarkUpDynamic()
+                                        .table[inside: MarkUpDynamic()
+                                                .tr[inside: MarkUpDynamic()
+                                                        .td[character: "一行目"]
+                                                        .td[character: "二行目"]
+                                                ]
+                                        ]
+                                ]
+                        ].generate(), "<html><body><table><tr><td>一行目</td><td>二行目</td></tr></table></body></html>")
     }
-
+    
+    func testAttributes() {
+        XCTAssertEqual(MarkUpDynamic()
+                        .html[inside: MarkUpDynamic()
+                                .body[inside: MarkUpDynamic()
+                                        .table[attributes: ["border" : "1"]][inside: MarkUpDynamic()
+                                                                                .tr[inside: MarkUpDynamic()
+                                                                                        .td[character: "一行目"]
+                                                                                        .td[character: "二行目"]
+                                                                                ]
+                                        ]
+                                ]
+                        ]
+                        .generate(), "<html><body><table border=\"1\"><tr><td>一行目</td><td>二行目</td></tr></table></body></html>")
+    }
+    
+    func testAddEndTag() {
+        XCTAssertEqual(MarkUpDynamic()
+                        .html[inside: MarkUpDynamic()
+                                .body[inside: MarkUpDynamic()
+                                        .br[addEndTag: false]
+                                        .br[addEndTag: false]
+                                        .br[addEndTag: false]
+                                ]
+                        ]
+                        .generate(), "<html><body><br><br><br></body></html>")
+    }
+    
+    func testXml() {
+        XCTAssertEqual(MarkUpDynamic(doctype: #"<?xml version="1.0" encoding="UTF-8"?>"#)
+                        .レシピ[inside: MarkUpDynamic()
+                                    .手順[character: "全ての材料を一緒にして混ぜます。"]
+                                    .手順[character: "オーブンに入れて温度を180℃にして30分間焼きます。"]
+                        ]
+                        .generate(), #"<?xml version="1.0" encoding="UTF-8"?><レシピ><手順>全ての材料を一緒にして混ぜます。</手順><手順>オーブンに入れて温度を180℃にして30分間焼きます。</手順></レシピ>"#)
+    }
+    
     static var allTests = [
         ("testExample", testExample),
+        ("testAttributes", testAttributes),
+        ("testAddEndTag", testAddEndTag),
+        ("testXml", testXml),
+        
     ]
 }
